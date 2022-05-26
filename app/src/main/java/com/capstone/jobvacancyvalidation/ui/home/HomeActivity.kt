934d32961da34855ff75ce5e50f1abd94b23a294
naofalx <naofalnirvana18@gmail.com>
2022-05-26 19:32:09 +0700

    rounded buttons etc

--- app/src/main/java/com/capstone/jobvacancyvalidation/ui/home/HomeActivity.kt
+++ app/src/main/java/com/capstone/jobvacancyvalidation/ui/home/HomeActivity.kt
@@ -3,11 +3,15 @@ package com.capstone.jobvacancyvalidation.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.jobvacancyvalidation.R
+import com.capstone.jobvacancyvalidation.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
     }
