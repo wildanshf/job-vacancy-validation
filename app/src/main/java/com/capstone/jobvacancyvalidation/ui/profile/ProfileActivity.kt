7a097edce1005afd007ce7a0c081609b1e140834
Wildan <dedan03@yahoo.co.id>
2022-05-28 18:31:07 +0700

    feat: Profile Page

---
+++ app/src/main/java/com/capstone/jobvacancyvalidation/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.jobvacancyvalidation.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.jobvacancyvalidation.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
