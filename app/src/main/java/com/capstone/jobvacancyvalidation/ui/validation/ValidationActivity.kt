04aa808675233288479e08372894c06a97b42f3b
Wildan <dedan03@yahoo.co.id>
2022-05-28 18:31:35 +0700

    feat: Validation Page

---
+++ app/src/main/java/com/capstone/jobvacancyvalidation/ui/validation/ValidationActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.jobvacancyvalidation.ui.validation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.jobvacancyvalidation.R
+
+class ValidationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_validation)
+    }
+}
