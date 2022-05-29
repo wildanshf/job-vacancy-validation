49f780b546f864a1d6b22bbac972e3f881766314
Wildan <dedan03@yahoo.co.id>
2022-05-29 17:13:06 +0700

    update: add view binding

--- app/src/main/java/com/capstone/jobvacancyvalidation/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/capstone/jobvacancyvalidation/ui/profile/ProfileActivity.kt
@@ -2,12 +2,15 @@ package com.capstone.jobvacancyvalidation.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.jobvacancyvalidation.R
+import com.capstone.jobvacancyvalidation.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
