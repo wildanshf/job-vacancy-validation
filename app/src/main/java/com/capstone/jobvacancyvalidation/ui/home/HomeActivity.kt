3f55cbe60115bb261728be6a616357b49abb6f24
naofalx <naofalnirvana18@gmail.com>
2022-05-29 16:51:00 +0700

    feat: splash screen style: history page layouts chore: update gradle dependencies

--- app/src/main/java/com/capstone/jobvacancyvalidation/ui/home/HomeActivity.kt
+++ app/src/main/java/com/capstone/jobvacancyvalidation/ui/home/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.capstone.jobvacancyvalidation.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.jobvacancyvalidation.R
 import com.capstone.jobvacancyvalidation.databinding.ActivityHomeBinding
+import com.capstone.jobvacancyvalidation.ui.login.LoginActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -14,5 +16,10 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.logoutButton.setOnClickListener {
+            val logoutIntent = Intent(this@HomeActivity, LoginActivity::class.java)
+            startActivity(logoutIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/capstone/jobvacancyvalidation/ui/splash/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.capstone.jobvacancyvalidation.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.capstone.jobvacancyvalidation.databinding.ActivitySplashBinding
+import com.capstone.jobvacancyvalidation.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }, DURATION)
+    }
+
+    companion object {
+        private const val DURATION = 3000L
+    }
+}
