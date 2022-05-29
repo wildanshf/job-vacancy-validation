64d601dc3055a24dbbf917c3b3a47019cd87da22
Wildan <dedan03@yahoo.co.id>
2022-05-29 16:53:09 +0700

    update: add back button in action bar

--- app/src/main/java/com/capstone/jobvacancyvalidation/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/capstone/jobvacancyvalidation/ui/profile/ProfileActivity.kt
@@ -8,5 +8,13 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/capstone/jobvacancyvalidation/ui/validation/ValidationActivity.kt
+++ app/src/main/java/com/capstone/jobvacancyvalidation/ui/validation/ValidationActivity.kt
@@ -8,5 +8,13 @@ class ValidationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_validation)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
