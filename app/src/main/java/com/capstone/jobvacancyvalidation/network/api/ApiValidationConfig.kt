83ba7cca83716bc119e73aedb3173dcaaad361fa
Wildan <dedan03@yahoo.co.id>
2022-06-09 21:31:20 +0700

    feat: api config for job validation

---
+++ app/src/main/java/com/capstone/jobvacancyvalidation/network/api/ApiValidationConfig.kt
@@ -0,0 +1,29 @@
+package com.capstone.jobvacancyvalidation.network.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ApiValidationConfig {
+    fun getApiService(): ApiService {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(90, TimeUnit.SECONDS)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://predict-evchg5m7eq-uc.a.run.app/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
+}
